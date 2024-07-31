65fc285878f5618ef59a9e77ed498a434277dd73
Shruti926 <166026243+Shruti926@users.noreply.github.com>
2024-07-31 13:59:55 +0530

    Time: 4 ms (100%), Space: 44.3 MB (11.13%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,33 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String str = Integer.toString(x);
-        if(x<0)
+        int copy=x;
+        int rev=0;
+        while(copy>0)
         {
-            return false;
+            int d=copy%10;
+            rev=rev*10+d;
+            copy=copy/10;
         }
-        char []arr = str.toCharArray();
-        int left = 0;
-        int right = arr.length - 1;
-        while(left < right)
-        {
-            char temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--;
-        }
-        String str_r = new String(arr);
-        
-            try{
-                int num_r = Integer.parseInt(str_r);
-                if(x == num_r)
-        {
-            return true;
-        }
-        }
-        catch(NumberFormatException e)
-        {return false; }
+        if(rev==x)
+        return true;
+        else
         return false;
-        }
-        
     }
+}
