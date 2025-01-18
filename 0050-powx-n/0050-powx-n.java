81823d29e2c9d9779a0342baffe935ec7896f107
Shruti926 <166026243+Shruti926@users.noreply.github.com>
2025-01-18 19:03:48 +0530

    Time: 0 ms (100%), Space: 43.3 MB (12.93%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,25 @@
 class Solution {
+    public double calcpower(double x, int n)
+    {
+        if(n == 0)
+        {
+            return 1;
+        }
+        double half = calcpower(x, n / 2); // Recursive step: calculate half power.
+        if (n % 2 == 0) {
+            return half * half; // If n is even.
+        } else {
+            return half * half * x; // If n is odd.
+        }
+    }
     public double myPow(double x, int n) {
-        double power  = Math.pow(x,n);
+        if (n<0)
+        {
+            n= n*-1;
+            return 1/calcpower(x,n);
+
+        }
+        double power = calcpower(x,n);
         return power;        
     }
 }
