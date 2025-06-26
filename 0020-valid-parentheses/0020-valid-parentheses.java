6c80f2a066c36ca8899f752a0c7fd79f63a58f66
Shruti926 <166026243+Shruti926@users.noreply.github.com>
2025-06-26 11:18:21 +0530

    Time: 3 ms (40%), Space: 41.6 MB (81.65%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,52 +1,26 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length()%2 != 0)
-        {
-            return false;
-        }
-        Stack<Character> st = new Stack() ;
-        for(int i = 0; i<s.length(); i++)
-        {
-            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[')
-            {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
                 st.push(s.charAt(i));
             }
-            else if(!st.empty() && s.charAt(i) == ')')
-            {
-                if(st.peek() == '(')
+            else {
+                if(st.isEmpty()){
+                    return false;
+                }
+                else if ((s.charAt(i) ==')' && st.peek() == '(') || 
+                (s.charAt(i) == '}' && st.peek() == '{') || 
+                (s.charAt(i) == ']' && st.peek() == '[')
+                )
                 {
                     st.pop();
                 }
-                else{
-                    st.push(s.charAt(i));
-                }
+                else return false;
             }
-            else if(!st.empty() && s.charAt(i) == '}')
-            {
-                if(st.peek() == '{')
-                {
-                    st.pop();
-                }
-                else{
-                    st.push(s.charAt(i));
-                }
-            }
-            else if(!st.empty() && s.charAt(i) == ']')
-            {
-                if(st.peek() == '[')
-                {
-                    st.pop();
-                }
-                else{
-                    st.push(s.charAt(i));
-                }
-            }
-            else{return false;}
         }
-        if(st.empty())
-        {
-            return true;
-        }
+        if(st.isEmpty())
+        {return true;}
         return false;
     }
 }
